04c9ec528e42005cf821597fd2981e7fa9b6c138
bsj0407-cmyk <bsj0407@gmail.com>
2026-01-05 18:53:56 +0900

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("백엔드 서버 정상 작동 중");
+});
+
+app.get("/health", (req, res) => {
+  res.send("OK");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on", PORT);
+});
